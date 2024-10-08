6ff4531487f145a02dd3e6d0357695f5904196e5
Manuel Lancharro Bayón <lancharro356@gmail.com>
2024-10-08 23:54:55 +0200

    Movimiento y TileMap

---
+++ scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+var direccion
+var moving = false
+const GRID_SIZE = 16
+@onready var ray: RayCast2D = $RayCast2D
+
+func _physics_process(delta: float) -> void:
+	direccion=Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		direccion= Vector2(1,0)
+		move()
+	elif Input.is_action_pressed("ui_left"):
+		direccion= Vector2(-1,0)
+		move()
+	elif Input.is_action_pressed("ui_up"):
+		direccion=Vector2(0,-1)
+		move()
+	elif Input.is_action_pressed("ui_down"):
+		direccion=Vector2(0,1)
+		move()
+	move_and_slide()
+
+func move()-> void:
+	if direccion:
+		
+		var next_tile: Vector2 = direccion * GRID_SIZE 
+		ray.target_position =next_tile
+		if(!ray.is_colliding()):
+			if moving==false:
+				moving=true
+				var tween = create_tween()
+				tween.tween_property(self,"position",position+direccion*GRID_SIZE, 0.25)
+				tween.tween_callback(moving_false)
+		else :
+			moving_false()
+			
+func moving_false()-> void:
+	moving=false
