8784d2f5053c2bdadbbe87911e7553151de2ce31
mlanbay <lancharro356@gmail.com>
2024-10-09 08:33:48 +0200

    Animaciones player

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,20 +4,25 @@ var direccion
 var moving = false
 const GRID_SIZE = 16
 @onready var ray: RayCast2D = $RayCast2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D 
 
 func _physics_process(delta: float) -> void:
 	direccion=Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
-		direccion= Vector2(1,0)
+		direccion= Vector2.RIGHT
+		animated_sprite.play("right")
 		move()
 	elif Input.is_action_pressed("ui_left"):
-		direccion= Vector2(-1,0)
+		direccion= Vector2.LEFT
+		animated_sprite.play("left")
 		move()
 	elif Input.is_action_pressed("ui_up"):
-		direccion=Vector2(0,-1)
+		direccion=Vector2.UP
+		animated_sprite.play("up")
 		move()
 	elif Input.is_action_pressed("ui_down"):
-		direccion=Vector2(0,1)
+		direccion=Vector2.DOWN
+		animated_sprite.play("down")
 		move()
 	move_and_slide()
 
@@ -28,6 +33,7 @@ func move()-> void:
 		ray.target_position =next_tile
 		if(!ray.is_colliding()):
 			if moving==false:
+				
 				moving=true
 				var tween = create_tween()
 				tween.tween_property(self,"position",position+direccion*GRID_SIZE, 0.25)
@@ -37,3 +43,4 @@ func move()-> void:
 			
 func moving_false()-> void:
 	moving=false
+	animated_sprite.stop()
